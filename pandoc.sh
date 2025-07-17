9873583477937be51e9d2a2f4723d6d49a435c20
zuzananeverilova <zuzana.neverilova@firma.seznam.cz>
2025-07-17 23:27:38 +0200

    columns

--- pandoc.sh
+++ pandoc.sh
@@ -19,4 +19,4 @@ OUTPUT=${DIR}.tex
 
 echo "Writing $INPUT to $OUTPUT"
 
-pandoc --columns 80 -f "$SOURCE_FORMAT"  -t beamer $INPUT -o $OUTPUT
+pandoc --columns 70 -f "$SOURCE_FORMAT"  -t beamer $INPUT -o $OUTPUT
